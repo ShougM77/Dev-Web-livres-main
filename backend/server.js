e7b0df52e6115801376642a4154e9e01dcfd958f
ShougM77 <mdge@hotmail.fr>
2024-09-05 19:03:27 +0200

    Phase 1

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware pour traiter les requêtes JSON
+app.use(express.json());
+
+// Route de test pour vérifier si le serveur fonctionne
+app.get('/', (req, res) => {
+    res.send('API Mon Vieux Grimoire fonctionne !');
+});
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur le port ${PORT}`);
+});
